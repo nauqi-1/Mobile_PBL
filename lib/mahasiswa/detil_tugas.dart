ba82db60759e6be7ee41e25332a122e4972cdca1
nauqi-1 <nauqi2208@gmail.com>
2024-10-16 14:11:57 +0700

    Modif Detil Tugas Mhs

--- lib/mahasiswa/detil_tugas.dart
+++ lib/mahasiswa/detil_tugas.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
-class MhsDetilTugas extends StatelessWidget {
+class MhsDetilTugas extends StatefulWidget {
   const MhsDetilTugas({super.key});
 
   @override
+  State<MhsDetilTugas> createState() => _MhsDetilTugasState();
+}
+
+class _MhsDetilTugasState extends State<MhsDetilTugas> {
   Widget build(BuildContext context) {
     // TODO: implement build
     throw UnimplementedError();
