06c16fac9faf94e8a58557a2c17bfdf4c051a264
Nairsya21 <nairsyasyauqighasila@gmail.com>
2024-10-16 14:05:39 +0700

    dsn notifikasi

---
+++ lib/dosen/detail_request.dart
@@ -0,0 +1,241 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DetailRequestPage extends StatefulWidget {
+  const DetailRequestPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<DetailRequestPage> createState() => _DetailRequestPageState();
+}
+
+class _DetailRequestPageState extends State<DetailRequestPage> {
+  void _handleAccept() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: const Text('Anda telah menerima request mahasiswa'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleReject() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: const Text('Anda telah menolak request mahasiswa'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0xff2d1b6b),
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  widget.title,
+                  style: const TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                const SizedBox(height: 4),
+                RichText(
+                  text: const TextSpan(children: [
+                    TextSpan(
+                        text: 'J',
+                        style: TextStyle(
+                            color: Colors.brown,
+                            fontSize: 19,
+                            fontWeight: FontWeight.bold)),
+                    TextSpan(
+                      text: 'T',
+                      style: TextStyle(
+                          color: Colors.orange,
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: 'I',
+                      style: TextStyle(
+                          color: Colors.yellow,
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: ' Polinema',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 19,
+                      ),
+                    ),
+                  ]),
+                )
+              ],
+            ),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.notifications_outlined,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 70,
+        child: Container(
+          color: const Color(0xff2d1b6b),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.home_outlined,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.list_sharp,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  CupertinoIcons.briefcase,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ),
+              IconButton(
+                onPressed: () {}, // Replace with _profileDsn if needed
+                icon: const Icon(
+                  Icons.account_circle_outlined,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            const Center(
+              child: Text(
+                'Judul Tugas Kompensasi',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ),
+            const Text('Nama Mahasiswa'),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text('NIM'),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text('Program Studi'),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text('Kelas'),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text('No. Hp'),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            const Text('Kompetensi'),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: _handleAccept,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    fixedSize: const Size(130, 40),
+                  ),
+                  child: const Text('Terima'),
+                ),
+                ElevatedButton(
+                  onPressed: _handleReject,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    fixedSize: const Size(130, 40),
+                  ),
+                  child: const Text('Tolak'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
